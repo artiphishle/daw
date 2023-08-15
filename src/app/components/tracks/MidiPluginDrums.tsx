50c71a9a80e646ac53317e5e40862547f973a089
scha-ch <fabio.gartenmann@protonmail.com>
2023-08-16 00:55:48 +0200

    #2 Show only Bassdrum, Snare, Closed Hihat for now

--- src/app/components/tracks/MidiPluginDrums.tsx
+++ src/app/components/tracks/MidiPluginDrums.tsx
@@ -1,9 +1,13 @@
 import { usedMidiChannels } from "./midi/constants/midiChannels.constants";
 
 export default function MidiPluginDrums() {
+  const tmpVisibleChannelNumbers = [36, 38, 42];
+  const tmpVisibleChannels = usedMidiChannels.filter((channel, channelIndex) =>
+    tmpVisibleChannelNumbers.includes(channelIndex + 35)
+  );
   return (
     <ul className="flex flex-col">
-      {usedMidiChannels.map((usedMidiChannel, channelIndex) => (
+      {tmpVisibleChannels.map((usedMidiChannel, channelIndex) => (
         <li key={`midi-channel-${channelIndex + 35}`}>
           {channelIndex + 35}: {Object.values(usedMidiChannel)}
         </li>
