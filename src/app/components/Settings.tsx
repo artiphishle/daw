441a998198285f4f9b5a2cc86e976240e6008cb6
scha-ch <fabio.gartenmann@protonmail.com>
2023-09-24 13:47:20 +0200

    DAW#10 Update to WebMidi

--- src/app/components/Settings.tsx
+++ src/app/components/Settings.tsx
@@ -1,6 +1,12 @@
+import useMidiDevice from "@/app/core/hooks/useMidiDevice";
+
 export default function Settings() {
+  // Enable MIDI Device (WebMidi)
+  useMidiDevice({ sysex: false });
+
   // TODO: Move to better place
   // TODO: Go over code again
+  /*
   const initializeDevice = (device: MIDIInput) => {
     device.onmidimessage = (ev: Event) => {
       const message = ev as MIDIMessageEvent;
@@ -10,14 +16,12 @@ export default function Settings() {
       console.log(command, note, velocity);
     };
   };
-
   const initialize = (access: MIDIAccess) => {
     const devices = Array.from(access.inputs.values());
     for (let device of devices) {
       initializeDevice(device);
     }
   };
-
   const requestAccess = (): Promise<MIDIAccess> => {
     return new Promise((resolve, reject) => {
       if (navigator.requestMIDIAccess)
@@ -25,7 +29,6 @@ export default function Settings() {
       else reject();
     });
   };
-
   const enableMidi = async () => {
     try {
       const access = await requestAccess();
@@ -34,16 +37,16 @@ export default function Settings() {
       console.error(error);
     }
   };
-
+*/
   return (
     <section className="bg-white p-4">
       <p>TODO</p>
-      <button
+      {/*<button
         onClick={enableMidi}
         className="inline-flex items-center justify-center px-8 py-4 bg-slate-300"
       >
-        Enable Midi Device
-      </button>
+        Enable Midi Device (Native)
+      </button>*/}
     </section>
   );
 }
