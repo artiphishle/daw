167852567f48a6ce26b51ab2004f6e4059a856cf
scha-ch <fabio.gartenmann@protonmail.com>
2023-09-29 07:48:38 +0200

    DAW#53 read API to get 'samples/' folder and show it in the <Browser /> component

--- src/app/components/Browser.tsx
+++ src/app/components/Browser.tsx
@@ -1,55 +1,33 @@
-// import propTypes from "prop-types";
-
 import { FileAudioIcon, FolderIcon, FolderOpenIcon } from "lucide-react";
+import useSWR from "swr";
+import { EEndpoint } from "../types/daw";
 
 const fileStyle = "w-4 h-4 mr-2 fill-green-200";
 const folderStyle = "w-4 h-4 mr-2 fill-blue-200";
 const liStyle = "flex";
 
+interface IThree {
+  publicSamples: string[];
+}
+
 // TODO extract to other file and load samples from server
-function Three() {
+function Three({ publicSamples }: IThree) {
   return (
     <ul>
       <li className={liStyle}>
-        <FolderIcon className={folderStyle} />
-        Folder 1
-      </li>
-      <li className={liStyle}>
-        <FolderIcon className={folderStyle} />
-        Folder 2
-      </li>
-      <li className={liStyle}>
-        <FolderIcon className={folderStyle} />
-        Presets
-      </li>
-      <li className={liStyle}>
         <FolderOpenIcon className={folderStyle} />
         Samples
-        <ul className="mt-6 -ml-16 pl-4 border-l border-gray-400">
-          <li className={liStyle}>
-            <FolderIcon className={folderStyle} />
-            Bank-01
-          </li>
-          <li className={liStyle}>
-            <FolderIcon className={folderStyle} />
-            Bank-02
-          </li>
-          <li className="flex">
-            <FolderIcon className={folderStyle} />
-            Bank-03
-          </li>
-          <li className="flex">
-            <FolderIcon className={folderStyle} />
-            Bank-04
-          </li>
-          <li className="flex">
-            <FolderIcon className={folderStyle} />
-            Bank-05
-          </li>
-          <li className="flex">
-            <FileAudioIcon className={fileStyle} />
-            halloween.mp3
-          </li>
+        <ul>
+          {publicSamples.map((name, dirIndex) => (
+            <li key={`browser-three-${dirIndex}`} className={liStyle}>
+              {name.endsWith(".wav") ? (
+                <FileAudioIcon className={folderStyle} />
+              ) : (
+                <FolderIcon className={folderStyle} />
+              )}
+              {name}
+            </li>
+          ))}
         </ul>
       </li>
     </ul>
@@ -57,18 +35,24 @@ function Three() {
 }
 
 function Browser() {
+  const { data, isLoading, error } = useSWR<string[], boolean, any>(
+    EEndpoint.Browser,
+    (endpoint: EEndpoint) => fetch(endpoint).then((res) => res.json())
+  );
+
+  /*
+  useEffect(() => {
+    if (data) console.log(data);
+  }, [data]);
+*/
   return (
     <section className="flex bg-white p-20 text-xs">
       <div className="pr-8">
-        <Three />
+        {data?.length && <Three publicSamples={data} />}
       </div>
       <div className="flex-1 bg-gray-50 p-20">Content</div>
     </section>
   );
 }
-
-Browser.propTypes = {
-  // TODO
-};
 
 export default Browser;
